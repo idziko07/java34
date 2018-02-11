d5b7d1cf1d53350d554256cd618eb764956cdfcf
Kacper Idzikowski <kacper.idzikowski@o2.pl>
2018-02-11 22:13:38 +0100

    zadanie java 34 z dodatkowym

--- src/App.java
+++ src/App.java
@@ -5,16 +5,19 @@ public class App {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         input.useLocale(Locale.US);
-        Directions[] directions = Directions.values();
+//        Directions[] directions = Directions.values();
 
-        System.out.println("Kierunki geograficzne: ");
-        for (Directions direction : directions) {
-            System.out.println(direction.name());
+//        System.out.println("Kierunki geograficzne: ");
+//        for (Directions direction : directions) {
+//            System.out.println(direction.name());
+//        }
+
+//        System.out.println("\nW jakim kierunku idziemy: ");
+//        String go = input.nextLine();
+//
+//        System.out.println(Directions.valueOf(go));
+
+        System.out.println(Directions.convert(2));
+
         }
-
-        System.out.println("\nW jakim kierunku idziemy: ");
-        String go = input.nextLine();
-
-        System.out.println(Directions.valueOf(go));
-    }
 }
--- src/Directions.java
+++ src/Directions.java
@@ -2,12 +2,32 @@ public enum Directions {
     NORTH("północ"),
     EAST("wschód"),
     SOUTH("południe"),
-    WEST("zachód");
+    WEST("zachód"),
+    UNDEFINED("nieznany");
 
     private String name;
     Directions(String name) {
         this.name = name;
     }
+
+
+
+    public static String convert(int num){
+
+        switch (num){
+            case 0:
+                return NORTH.name();
+            case 1:
+                return EAST.name();
+            case 2:
+                return SOUTH.name();
+            case 3:
+                return WEST.name();
+                default:
+                    return UNDEFINED.name();
+        }
+    }
+
 
     @Override
     public String toString() {
